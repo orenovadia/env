c4efcef1e433f05b92610cbaf1374f59041dc384
orenovadia <orenovad@gmail.com>
2017-05-27 20:33:35 +0300

    Reformats

--- install/config_git.sh
+++ install/config_git.sh
@@ -1,9 +1,10 @@
+#!/usr/bin/env bash
+
 git config --global core.editor "vim"
 
 # Aliases
-
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit" 
 git config --global alias.lgs "log --color --stat --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit" 
 git config --global alias.ss "status --short --branch" 
 git config --global alias.stats "shortlog -sn" 
-git config --global alias.recent "for-each-ref --count=10 --sort=-committerdate refs/heads/ --format='%(refname:short)'" 
+git config --global alias.recent "for-each-ref --count=10 --sort=-committerdate refs/heads/ --format='%(refname:short)'"
