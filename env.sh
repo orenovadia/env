4488cb1eff9e2a57b1fa687a1083785d7f993ab0
orenovadia <oren@twiggle.com>
2017-05-28 09:12:37 +0300

    Merge environment from work

--- env.sh
+++ env.sh
@@ -6,14 +6,37 @@ alias sa="source ~/env/env.sh "
 
 # aliases
 alias cls="clear"
+alias cb='xclip -sel clip'
+alias yaml2json="ruby -ryaml -rjson -e 'puts JSON.pretty_generate(YAML.load(ARGF))'"
+alias json2yaml="ruby -ryaml -rjson -e 'puts JSON.load(ARGF).to_yaml'"
+alias pycharm="/opt/pycharm/bin/pycharm.sh>/dev/null"
+alias csv="column -t -s,"
+alias del_pyc="find . -name '*pyc' -delete"
+
 
 # git
 alias gchange='select i in `git recent`  ; do echo checking out  $i && git checkout $i ; git ss ; break; done'
 
+garchive() {
+	for branch in "$@"
+	do
+		echo archiving "${branch}"
+		git tag archive/${branch} ${branch}
+		git branch -D ${branch}
+	done
+}
+
 # functions
 function ap {
-   awk -v x=$1 '{print $x}' 
+   awk -v x=$1 '{print $x}'
 }
+abs_path(){
+	readlink -f $1
+}
+
+# configuration variables
+HISTSIZE=20000
+HISTFILESIZE=20000
 
 # local sources
 if [ -d ~/env/local/ ]; then
@@ -26,6 +49,5 @@ fi
 if [ -f ~/.git-completion.bash ]; then
   . ~/.git-completion.bash
 fi
-
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
-
+[ -f /usr/local/bin/virtualenvwrapper.sh ] && source /usr/local/bin/virtualenvwrapper.sh
