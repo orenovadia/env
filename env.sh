1b49d7886159bd630d8cd063f91da1d3868f4a7e
orenovadia <oren@twiggle.com>
2017-08-21 14:45:57 +0300

    Added alias: cd to git root directory -> groot

--- env.sh
+++ env.sh
@@ -17,6 +17,7 @@ alias noti='~/env/scripts/noti'
 
 # git
 alias gchange='git recent  | fzf --height 40% | xargs -t --no-run-if-empty git checkout'
+alias groot='cd `git rev-parse --show-toplevel`'
 
 mgit(){
 for i in `ls -d  */.git  | sed 's/\/\.git//g'`; do echo "For ${i}:"; cd $i; git $@; echo "-----------------"; cd ..; done
