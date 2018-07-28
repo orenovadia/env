bea9d117ad90eadde1ac7f6496f593ca181e5456
orenovadia <oren@twiggle.com>
2018-07-28 14:32:56 -0700

    Added misc commands

---
+++ sources/misc_commands.sh
@@ -0,0 +1,19 @@
+function __misc_cmds() {
+# simply add here commands where 
+# the first word is the name (and will be omited) 
+# and the rest is the command
+cat << EOM
+weather curl http://wttr.in?1
+cheats curl http://cheat.sh
+EOM
+}
+
+function __misc_choose() {
+__misc_cmds | fzf --reverse --ansi --height 20%  --with-nth 1 | sed  's,^\w\w*\s*,,g' 
+}
+
+function misc() {
+    V=`__misc_choose`
+    echo ${V} 1>&2
+    ${V}
+}
