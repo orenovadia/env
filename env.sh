224e95f66fa495f425c1ec0d2c0ddcfc3fe90b91
orenovadia <oren@twiggle.com>
2018-09-07 15:49:47 -0700

    exit code in prompt

--- env.sh
+++ env.sh
@@ -50,7 +50,7 @@ export EDITOR='vi'
 parse_git_branch() {
  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
-PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\]$(parse_git_branch)\[\033[00m\]\$ '
+PS1='$(__E=$? ; if [ $__E == 0 ]; then true; else echo "(exit:$__E) "; fi)${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\]$(parse_git_branch)\[\033[00m\]\$ '
    
 # local sources
 if [ -d ~/env/local/ ]; then
