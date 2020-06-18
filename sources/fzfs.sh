8ddcae63cbeffbef0535b1b04587d75167be407a
orenovadia <orenovad@gmail.com>
2020-06-18 13:50:31 -0700

    use fd if available

--- sources/fzfs.sh
+++ sources/fzfs.sh
@@ -1,29 +1,14 @@
 #!/usr/bin/env bash
-# Python:
-function _grep_fzf(){ 
-PATTERN=$1
-ref=$( grep  --include '*.py' -o -n  -r -P "${PATTERN}" . \
-| fzf --height 40%  -d':' -n1 --with-nth 3 )
 
-[[ -z $ref ]] &&  return
-vi `echo $ref  |  awk -F':' '{print "+"$2 ,$1}' ` ;
-}; 
+export FZF_DEFAULT_OPTS="--ansi"
 
-function _grep_fzf_file_ref(){ 
-PATTERN=$1
-ref=`grep  --include '*.py' -o -n  -r -P "${PATTERN}" . | grep -v test | awk -F':' '{print $3, $1, $2}'| fzf --height 40% `
-# name file line
-[[ -z $ref ]] &&  return
-vi `echo $ref  |  awk '{print "+"$3 ,$2}' ` ;
-}; 
-function fspymethod(){
-_grep_fzf_file_ref  '(?<=def\s)(\b\w+\b)'
-}
-function fspyclass(){
-_grep_fzf  '(?<=class\s)(\b\w+)'
-}
-bind '"\C-n":"fspyclass\n"'  # Ctrl+n opens `fspyclass`
-bind '"\C-f":"fspymethod\n"' # Ctrl+f opens `pymethod`
+# Use fd for fzf if available
+if hash fd 2>/dev/null; then
+   # https://github.com/sharkdp/fd#using-fd-with-fzf
+   export FZF_DEFAULT_COMMAND='fd --follow --hidden --exclude .git --color=always'
+   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+fi
+
 
 # Git:
 
@@ -41,6 +26,33 @@ bind '"\C-b":"gchange\n"' # Ctrl+b opens changes git branch
 bind '"\C-g":"gchange\n"' # Ctrl+g opens changes git branch
 
 
+# Python:
+function _grep_fzf(){ 
+PATTERN=$1
+ref=$( grep  --include '*.py' -o -n  -r -P "${PATTERN}" . \
+| fzf --height 40%  -d':' -n1 --with-nth 3 )
+
+[[ -z $ref ]] &&  return
+vi `echo $ref  |  awk -F':' '{print "+"$2 ,$1}' ` ;
+}; 
+
+function _grep_fzf_file_ref(){ 
+PATTERN=$1
+ref=`grep  --include '*.py' -o -n  -r -P "${PATTERN}" . | grep -v test | awk -F':' '{print $3, $1, $2}'| fzf --height 40% `
+# name file line
+[[ -z $ref ]] &&  return
+vi `echo $ref  |  awk '{print "+"$3 ,$2}' ` ;
+}; 
+
+function fspymethod(){
+_grep_fzf_file_ref  '(?<=def\s)(\b\w+\b)'
+}
+function fspyclass(){
+_grep_fzf  '(?<=class\s)(\b\w+)'
+}
+# bind '"\C-n":"fspyclass\n"'  # Ctrl+n opens `fspyclass`
+# bind '"\C-f":"fspymethod\n"' # Ctrl+f opens `pymethod`
+
 function fspyclassreference () 
 { 
     ref=$( grep  --include '*.py' -o -r -P "${PATTERN}" .     | fzf --height 40%  -d':' -n1 --with-nth 2 );
@@ -49,3 +61,4 @@ function fspyclassreference ()
 }
 
 alias fspy='$(echo fspymethod,fspyclass,fspyclassreference |tr "," "\n"|fzf --height 10%)'
+
