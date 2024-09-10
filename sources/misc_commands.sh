f25be7fb97c53643df16de359a3619fb6d95f810
orenovadia <orenovad@gmail.com>
2024-09-10 12:37:49 -0700

    bart script fix

--- sources/misc_commands.sh
+++ sources/misc_commands.sh
@@ -20,8 +20,11 @@ function misc() {
 }
 
 function bart() {
-    echo "ETA of BART from Mont. station to Antioch:"
-    curl -s 'https://api.bart.gov/api/etd.aspx?cmd=etd&orig=MONT&key=MW9S-E7SL-26DU-VV8V&json=y' | jq '.root.station[0].etd[] | select(.destination == "Antioch").estimate[] | {minutes: .minutes, delay:.delay}'
+    echo "ETA of BART from Mont. station to Antioch or Pits:"
+    (
+    echo "min delay"
+     curl -s 'https://api.bart.gov/api/etd.aspx?cmd=etd&orig=MONT&key=MW9S-E7SL-26DU-VV8V&json=y' | jq -r '.root.station[0].etd[] | select(.destination == "Antioch" or .destination == "Pittsburg/Bay Point").estimate[] | [(.minutes | tonumber), .delay] | @tsv'  | sort -g 
+ ) | column -t 
 }
 
 
